2352bbefda74af9a10faa7110585da4f0f09c803
Roger Grajales <70453253+rogradev@users.noreply.github.com>
2026-05-27 22:41:50 +0000

    Deleted Files

--- program/programs/quorum/src/instructions/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod initialize_platform;
-pub mod create_project;
-pub mod social_vote;
-pub mod contribute;
-pub mod finalize_funding;
-pub mod refund;
-pub mod dev_activity;
--- program/programs/quorum/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod platform;
-pub mod project;
-pub mod contribution;
-pub mod vote;
-
-pub use platform::*;
-pub use project::*;
-pub use contribution::*;
-pub use vote::*;
